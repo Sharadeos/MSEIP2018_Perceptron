faf7b6e2317b59c9e6223e1b4951eaae04606acd
Sharadeos <shadow_zeny@hotmail.com>
2018-06-13 12:30:52 -0700

    Perceptron Files

--- perceptron.cpp
+++ perceptron.cpp
@@ -10,12 +10,7 @@ int main(int argc, char *argv[]) {
 	*/
 	std::vector< std::vector<double> > data;	// Vector containing data and labels
 	std::ifstream file( argv[1] );			// File to load data
-	int tp = 0;					// True positives
-	int p = 0;					// Number of positives
-	int n = 0;					// Number of negatives
-	int tn = 0;					// True negatives
-	int fn = 0;					// False negatives
-	int fp = 0;					// False positives
+
 
 	/* 
 	* Part 0: Code to load in from a CSV, no need to edit this
@@ -63,47 +58,15 @@ int main(int argc, char *argv[]) {
 		/* 
 		* Part 1.1: Reflex rule
 		*/
-		// If the sepal width (column 2) is geq to 3.0, predict 1 (versicolor)
-		std::cout << " and reflex agent predicts ";
-		if (row[3] >= 1.0) 
-			prediction = 1;
-		if (row[2] >= 2.0) 
-			prediction = 1;
-		std::cout << prediction << "." << std::endl;
 
 		/* 
 		* Part 1.2: Reflex rule
 		*/
-		// Increment the number of true positives
-		if ( label == -1 && prediction == -1 )
-			tp++;
-		
-		if ( label == 1 && prediction == 1 )
-			tn++;
 
-		if ( label == -1 && prediction == 1 )
-			fp++;
-
-		if ( label == 1 && prediction == -1 )
-			fn++;
-
-		
-		// Count the number of positives
-		if ( label == -1 )
-			p++;
-		if ( label == 1 )
-			n++;
 	}
 
 	/* 
 	* Part 2: Performance metrics
 	*/
-	std::cout << "True positives: " << tp << std::endl;
-	std::cout << "True negatives: " << tn << std::endl;
-	std::cout << "Num. positives: " << p << std::endl;
-	std::cout << "Num. negatives: " << n << std::endl;
-	std::cout << "Sensitivity/True positive rate: " << (double) tp / p * 100 << '%' <<  std::endl;
-	std::cout << "Precision: " << (double) tp/(tp+fp) * 100 << '%' <<  std::endl;
-	std::cout << "Specificity: " << (double) tn/n * 100 << '%' << std::endl;
-	std::cout << "Accuracy: " << (double) (tp+tn)/(tp+tn+fp+fn) *100 << '%' <<  std::endl;
+	
 	}
